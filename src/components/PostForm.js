b437e700bc36f50163a687f871fcc9edcb3fc6af
lakshya sharma <84004188+luckybossyt@users.noreply.github.com>
2022-06-12 12:28:43 +0530

    Update PostForm.js

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,89 +1,107 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React,{useState} from "react";
+import Axios from "axios";
 
-function PostF() {
+function PostForm(){
     const url="https://mockrestapi.herokuapp.com/api/employee"
-    const [data,setData]=useState({
-        id: "",
-        name: "",
-        phone: "",
-        email: "",
-        age: "",
-        country: "",
-        address: ""
-    })
+    const[data,setData]=useState({
+        id:"",
+        name:"",
+        phone:"",
+        email:"",
+        country:"",
+        age:"",
+        address:""
 
-    function submit(e){
+
+    })
+    function add(e){
         e.preventDefault();
-        axios.post(url,{
-        id: parseInt(data._id),
-        name: data.name,
-        phone: data.phone,
-        email: data.email,
-        age: data.age,
-        country: data.country,
-        address: data.address
+        Axios.post(url,{
+            id:parseInt(data._id),
+            
+            name:data.name,
+            email:data.email,
+            phone:data.phone,
+            address:data.address,
+            country:data.country,
+            age:data.age,
+            
+
         })
         .then(res=>{
             console.log(res.data)
         })
+
     }
+    
 
     function handle(e){
-        const newdata = { ...data }
-        newdata[e.target.id] = e.target.value
+        const newdata={...data}
+        newdata[e.target.id]=e.target.value
         setData(newdata)
         console.log(newdata)
+
     }
-  return (
-    <div>
-        <h2>Add an Employee</h2>
-      <form onSubmit={(e)=> submit(e)}>
-      <input onChange={(e) => handle(e)} id="id" value={data.id}
-          type="text"
-          name="id"
-          required="required"
-          placeholder="Enter id..."
-        ></input>
-        <input onChange={(e) => handle(e)} id="name" value={data.name}
-          type="text"
-          name="name"
-          required="required"
-          placeholder="Enter a name..."
-        ></input>
-        <input onChange={(e) => handle(e)} id="phone" value={data.phone}
-          type="tel"
-          name="phone number"
-          required="required"
-          placeholder="Enter a phone number..."
-        ></input>
-        <input onChange={(e) => handle(e)} id="email" value={data.email}
-          type="email"
-          name="email"
-          required="required"
-          placeholder="Enter an email..."
-        ></input>
-        <input onChange={(e) => handle(e)} id="age" value={data.age}
-          type="number"
-          name="age"
-          required="required"
-          placeholder="Enter the age..."
-        ></input>
-        <input onChange={(e) => handle(e)} id="country" value={data.country}
-          type="text"
-          name="country"
-          required="required"
-          placeholder="Enter the country name..."
-        ></input>
-        <input onChange={(e) => handle(e)} id="address" value={data.address}
-          type="text"
-          name="address"
-          required="required"
-          placeholder="Enter a address..."
-        ></input>
-        <button>Submit</button>
-      </form>
-    </div>
-  );
-}
-export default PostF
+    return(
+        <div>
+            <h2>add new information</h2>
+            <form onSubmit={(e)=> add(e)}>
+
+            <input
+                    onChange={(e) => handle(e)} id="_id" value={data._id}
+                    type="text"
+                    name="id"
+                    required="required"
+                    placeholder="Enter Id..">
+                </input>
+                <input
+                    onChange={(e) => handle(e)} id="name" value={data.name}
+                    type="text"
+                    name="name"
+                    required="required"
+                    placeholder="Enter Name..">
+                </input>
+                <input
+                    onChange={(e) => handle(e)} id="email" value={data.email}
+                    type="email"
+                    name="email"
+                    required="required"
+                    placeholder="Enter email..">
+                </input>
+                <input
+                    onChange={(e) => handle(e)} id="phone" value={data.phone}
+                    type="tel"
+                    name="phone"
+                    required="required"
+                    placeholder="Enter Phone No." >
+                </input>
+                <input
+                    onChange={(e) => handle(e)} id="address" value={data.address}
+                    ype="text"
+                    name="address"
+                    required="required"
+                    placeholder="Enter Address.." >
+                </input>
+                <input
+                    onChange={(e) => handle(e)} id="country" value={data.country}
+                    type="text"
+                    name="country"
+                    required="required"
+                    placeholder="Enter Country.." >
+                </input>
+                <input
+                    onChange={(e) => handle(e)} id="age" value={data.age}
+                    type="age"
+                    name="age"
+                    required="required"
+                    placeholder="Enter Age.." >
+                </input>
+                
+                <button>Add</button>
+            
+            </form>
+        </div>
+        
+    );
+};
+export default PostForm;
